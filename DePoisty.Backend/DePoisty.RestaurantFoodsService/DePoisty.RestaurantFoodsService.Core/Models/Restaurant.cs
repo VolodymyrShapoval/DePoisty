e77258246bd04a244d710457734deef8a9caca49
VolodymyrShapoval <volodymyr.sh.05@gmail.com>
2025-09-09 00:46:48 +0300

    fix: restore RestaurantMeta property and remove RestaurantMetaId from Restaurant model

--- DePoisty.Backend/DePoisty.RestaurantFoodsService/DePoisty.RestaurantFoodsService.Core/Models/Restaurant.cs
+++ DePoisty.Backend/DePoisty.RestaurantFoodsService/DePoisty.RestaurantFoodsService.Core/Models/Restaurant.cs
@@ -3,13 +3,13 @@
     public class Restaurant
     {
         public required Guid Id { get; set; }
-        public required Guid RestaurantMetaId { get; set; }
-        public RestaurantMeta RestaurantMeta { get; set; } = null!;
         public required string Name { get; set; }
         public required string Address { get; set; }
         public required string Website { get; set; }
         public required decimal QualityRating { get; set; }
 
         public ICollection<Dish> Dishes { get; set; } = new List<Dish>();
+
+        public RestaurantMeta RestaurantMeta { get; set; } = null!;
     }
 }
