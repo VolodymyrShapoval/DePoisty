2f12298ddb1da46352b2fe160462f5197a52f506
VolodymyrShapoval <volodymyr.sh.05@gmail.com>
2025-09-08 23:17:41 +0300

    feat: add RepositoryResult class and IRepository interface for handling repository operations

---
+++ DePoisty.Backend/DePoisty.RestaurantFoodsService/DePoisty.RestaurantFoodsService.Core/Common/RepositoryResult.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DePoisty.RestaurantFoodsService.Core.Common
+{
+    public class RepositoryResult
+    {
+        public bool IsSuccess { get; set; }
+        public string? ErrorMessage { get; set; }
+        public static RepositoryResult Success()
+        {
+            return new RepositoryResult { IsSuccess = true };
+        }
+        public static RepositoryResult Failure(string errorMessage)
+        {
+            return new RepositoryResult { IsSuccess = false, ErrorMessage = errorMessage };
+        }
+    }
+}
---
+++ DePoisty.Backend/DePoisty.RestaurantFoodsService/DePoisty.RestaurantFoodsService.Core/Interfaces/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using DePoisty.RestaurantFoodsService.Core.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DePoisty.RestaurantFoodsService.Core.Interfaces
+{
+    public interface IRepository<T>
+    {
+        Task<T> GetByIdAsync<T>(Guid id);
+        Task<IEnumerable<T>> GetAllAsync<T>();
+        Task<RepositoryResult> AddAsync<T>(T entity);
+        Task<RepositoryResult> UpdateAsync<T>(T entity);
+        Task<RepositoryResult> DeleteAsync<T>(T entity);
+    }
+}
