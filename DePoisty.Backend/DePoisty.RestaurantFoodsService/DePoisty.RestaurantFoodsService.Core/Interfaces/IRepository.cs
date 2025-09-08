5c87608d4c2c22fbc3e2f1f1a0ea37ba0d15a6e9
VolodymyrShapoval <volodymyr.sh.05@gmail.com>
2025-09-08 23:24:22 +0300

    refactor: simplify IRepository interface by removing generic type parameters from method signatures

--- DePoisty.Backend/DePoisty.RestaurantFoodsService/DePoisty.RestaurantFoodsService.Core/Interfaces/IRepository.cs
+++ DePoisty.Backend/DePoisty.RestaurantFoodsService/DePoisty.RestaurantFoodsService.Core/Interfaces/IRepository.cs
@@ -1,11 +1,13 @@
-﻿namespace DePoisty.RestaurantFoodsService.Core.Interfaces
+﻿using DePoisty.RestaurantFoodsService.Core.Common;
+
+namespace DePoisty.RestaurantFoodsService.Core.Interfaces
 {
     public interface IRepository<T>
     {
-        Task<T> GetByIdAsync<T>(Guid id);
-        Task<IEnumerable<T>> GetAllAsync<T>();
-        Task<RepositoryResult> AddAsync<T>(T entity);
-        Task<RepositoryResult> UpdateAsync<T>(T entity);
-        Task<RepositoryResult> DeleteAsync<T>(T entity);
+        Task<T> GetByIdAsync(Guid id);
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<RepositoryResult> AddAsync(T entity);
+        Task<RepositoryResult> UpdateAsync(T entity);
+        Task<RepositoryResult> DeleteAsync(T entity);
     }
 }
