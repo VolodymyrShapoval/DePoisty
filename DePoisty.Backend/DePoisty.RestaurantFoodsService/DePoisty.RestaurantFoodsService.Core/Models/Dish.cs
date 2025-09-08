daff5666767c7b94bc2a56adeaa7bc39eca09339
VolodymyrShapoval <volodymyr.sh.05@gmail.com>
2025-09-08 22:36:21 +0300

    fix: remove PricePerHundredGrams property from Dish model

--- DePoisty.Backend/DePoisty.RestaurantFoodsService/DePoisty.RestaurantFoodsService.Core/Models/Dish.cs
+++ DePoisty.Backend/DePoisty.RestaurantFoodsService/DePoisty.RestaurantFoodsService.Core/Models/Dish.cs
@@ -8,6 +8,5 @@
         public required decimal Price { get; set; }
         public required decimal Weight { get; set; }
         public string Description { get; set; } = string.Empty;
-        public decimal PricePerHundredGrams { get; set; }
     }
 }
