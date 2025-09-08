2fff6cb42bd559f879f3485369831bd88feac7e4
VolodymyrShapoval <volodymyr.sh.05@gmail.com>
2025-09-08 22:07:27 +0300

    fix: add missing 'Name' property to Category model

--- DePoisty.Backend/DePoisty.RestaurantFoodsService/DePoisty.RestaurantFoodsService.Core/Models/Category.cs
+++ DePoisty.Backend/DePoisty.RestaurantFoodsService/DePoisty.RestaurantFoodsService.Core/Models/Category.cs
@@ -3,6 +3,7 @@
     public class Category
     {
         public required string Id { get; set; }
+        public required string Name { get; set; }
         public ICollection<Dish> Dishes { get; set; } = new List<Dish>();
     }
 }
