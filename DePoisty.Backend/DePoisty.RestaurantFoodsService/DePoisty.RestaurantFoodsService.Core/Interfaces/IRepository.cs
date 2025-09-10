9cce4d243fc79c71f1073cef32b95b338948009a
VolodymyrShapoval <volodymyr.sh.05@gmail.com>
2025-09-11 01:19:22 +0300

    feat: implement RestaurantRepository with CRUD operations for Restaurant model

--- DePoisty.Backend/DePoisty.RestaurantFoodsService/DePoisty.RestaurantFoodsService.Core/Interfaces/IRepository.cs
+++ DePoisty.Backend/DePoisty.RestaurantFoodsService/DePoisty.RestaurantFoodsService.Core/Interfaces/IRepository.cs
@@ -4,7 +4,7 @@ namespace DePoisty.RestaurantFoodsService.Core.Interfaces
 {
     public interface IRepository<T>
     {
-        Task<T> GetByIdAsync(Guid id);
+        Task<T?> GetByIdAsync(Guid id);
         Task<IEnumerable<T>> GetAllAsync();
         Task<RepositoryResult> AddAsync(T entity);
         Task<RepositoryResult> UpdateAsync(T entity);
---
+++ DePoisty.Backend/DePoisty.RestaurantFoodsService/DePoisty.RestaurantFoodsService.Persistence/Data/Repositories/RestaurantRepository.cs
@@ -0,0 +1,50 @@
+﻿using DePoisty.RestaurantFoodsService.Core.Common;
+using DePoisty.RestaurantFoodsService.Core.Interfaces;
+using DePoisty.RestaurantFoodsService.Core.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DePoisty.RestaurantFoodsService.Persistence.Data.Repositories
+{
+    public class RestaurantRepository : IRepository<Restaurant>
+    {
+        private readonly AppDbContext _context;
+        public RestaurantRepository(AppDbContext context) => _context = context;
+
+        public async Task<RepositoryResult> AddAsync(Restaurant entity)
+        {
+            await _context.Restaurants.AddAsync(entity);
+            var result = await _context.SaveChangesAsync();
+            return (result > 0) ? RepositoryResult.Success() : RepositoryResult.Failure("Cannot add the entity to DB");
+        }
+
+        public async Task<RepositoryResult> DeleteAsync(Restaurant entity)
+        {
+            _context.Restaurants.Remove(entity);
+            var result = await _context.SaveChangesAsync();
+            return result > 0 ? RepositoryResult.Success() : RepositoryResult.Failure("Cannot delete the entity to DB");
+        }
+
+        public async Task<IEnumerable<Restaurant>> GetAllAsync()
+        {
+            return await _context.Restaurants.ToListAsync();
+        }
+
+        public async Task<Restaurant?> GetByIdAsync(Guid id)
+        {
+            return await _context.Restaurants.FirstOrDefaultAsync(r => r.Id == id);
+        }
+
+        public async Task<RepositoryResult> UpdateAsync(Restaurant entity)
+        {
+            var oldEntity = await _context.Restaurants.FirstOrDefaultAsync(r => r.Id == entity.Id);
+            if (oldEntity == null)
+            {
+                return RepositoryResult.Failure("Cannot find the entity to update");
+            }
+
+            _context.Restaurants.Update(entity);
+            var result = await _context.SaveChangesAsync();
+            return result > 0 ? RepositoryResult.Success() : RepositoryResult.Failure("Cannot delete the entity to DB");
+        }
+    }
+}
