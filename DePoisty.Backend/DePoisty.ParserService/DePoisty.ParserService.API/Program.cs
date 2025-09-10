9b67feb9cdf5c51c505a288eaefee564f3d0e190
Vektor19 <viktor.komissarov@knu.ua>
2025-09-10 21:52:38 +0300

    feat: add ParsingController and implement restaurant parsing endpoint; remove WeatherForecastController and related files

--- DePoisty.Backend/DePoisty.ParserService/DePoisty.ParserService.API/Program.cs
+++ DePoisty.Backend/DePoisty.ParserService/DePoisty.ParserService.API/Program.cs
@@ -1,6 +1,9 @@
+using DePoisty.ParserService.Application;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddApplicationServices();
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
